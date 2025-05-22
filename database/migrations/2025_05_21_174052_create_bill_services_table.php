68e7fbeac4cd0b3243bd3a08334c20ffa78c10bd
Your Name <you@example.com>
2025-05-22 09:08:36 -0700

    migration: Add Migrations

---
+++ database/migrations/2025_05_21_174052_create_bill_services_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bill_services', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('bill_id')->constrained('bills')->onDelete('cascade');
+    $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+    $table->double('charge');
+    $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bill_services');
+    }
+};
